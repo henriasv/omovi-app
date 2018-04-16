5aa657390023a675dea8eafcb4399f254bd03226
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2018-04-16 21:25:04 +0200

    initial commit with timestep controller

---
+++ src/components/FrameController.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block">
+    <span class="demonstration">Default value</span>
+    <el-slider v-model="frame" :min="min" :max="max"></el-slider>
+    <el-input-number size="mini" v-model="frame" :min="min" :max="max"></el-input-number>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FrameController',
+  props: ['uicontroller'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      frame: 0,
+      min: 0,
+      max: 0
+    }
+  },
+  methods: {
+    formatTooltip (val) {
+      return val / 100
+    }
+  },
+  watch: {
+    frame: function () {
+      this.uicontroller.setFrame(this.frame)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/UIController.vue
@@ -0,0 +1,29 @@
+<template>
+  <frame-controller :uicontroller="this"></frame-controller>
+</template>
+
+<script>
+import FrameController from './FrameController'
+export default {
+  name: 'UIController',
+  data () {
+    return {
+      msg: 'This is the container',
+      value1: 3
+    }
+  },
+  methods: {
+    setFrame (value) {
+      // this.omoviScene.setFrame(value)
+      console.log('omoviscene.setFrame call shall be implemented here')
+    }
+  },
+  components: {
+    FrameController
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
